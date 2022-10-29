7f89463f6ac394795236bc94a85f32e6d58edda1
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-10-29 09:56:01 +0300

    #12

--- UI/VolumeControl.cs
+++ UI/VolumeControl.cs
@@ -7,8 +7,14 @@ public class VolumeControl : MonoBehaviour
     [SerializeField] private Slider _slider;
     [SerializeField] private AudioMixer _audioMixer;
 
+    private float _minSliderValue = -20f;
+    private float _minMasterValume = -80f;
+    
     public void OnSetVolume()
     {
         _audioMixer.SetFloat("MasterVolume", _slider.value);
+
+        if (_slider.value < _minSliderValue)
+            _audioMixer.SetFloat("MasterVolume", _minMasterValume);
     }
 }
