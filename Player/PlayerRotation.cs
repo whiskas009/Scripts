5be59cd056949efee49e94f5c4f5613a54f44ac8
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-10-29 09:29:27 +0300

    #9

--- Player/PlayerRotation.cs
+++ Player/PlayerRotation.cs
@@ -5,12 +5,13 @@ public class PlayerRotation : MonoBehaviour
     [SerializeField] private float _rotationSmoothTime;
 
     private float _rotationSmoothVelocity;
+    private float _lockAngleValue = 0.0f;
 
     public void Rotate(float targetAngle)
     {
         float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,
                 targetAngle, ref _rotationSmoothVelocity, _rotationSmoothTime);
 
-        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        transform.rotation = Quaternion.Euler(_lockAngleValue, angle, _lockAngleValue);
     } 
 }
